6a6724a5f6ce6c4f03ab80fcba01a3d82cbb81a2
ADI <adiletaktay@gmail.com>
2024-04-08 22:23:08 +0500

    add news-blocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
-      <div>
-        
+      <div className='container'>
+        <header>
+          <h1 className="header-title">NEWS</h1>
+        </header>
+        <div className='news-block-wrapper'>
+          <div className="news-block">
+            <img  src="https://tengrinews.kz/userdata/news/2024/news_531610/thumb_m/photo_468156.jpeg" className='news-block__img' alt='news'></img>
+            <h2 className='news-block__title'>Lorem ipsum dolor sit</h2>
+            <div className='news-block__subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rem atque dicta maiores magnam laborum odio quaerat, possimus molestiae nostrum repudiandae. Illum exercitationem doloremque, asperiores fugiat aliquid autem quaerat incidunt!</div>
+            <div className='nws-block__date'>2024-04-08</div>
+          </div>
+          <div className="news-block">
+            <img  src="https://tengrinews.kz/userdata/news/2024/news_531610/thumb_m/photo_468156.jpeg" className='news-block__img' alt='news'></img>
+            <h2 className='news-block__title'>Lorem ipsum dolor sit</h2>
+            <div className='news-block__subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rem atque dicta maiores magnam laborum odio quaerat, possimus molestiae nostrum repudiandae. Illum exercitationem doloremque, asperiores fugiat aliquid autem quaerat incidunt!</div>
+            <div className='nws-block__date'>2024-04-08</div>
+          </div>
+          <div className="news-block">
+            <img  src="https://tengrinews.kz/userdata/news/2024/news_531610/thumb_m/photo_468156.jpeg" className='news-block__img' alt='news'></img>
+            <h2 className='news-block__title'>Lorem ipsum dolor sit</h2>
+            <div className='news-block__subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rem atque dicta maiores magnam laborum odio quaerat, possimus molestiae nostrum repudiandae. Illum exercitationem doloremque, asperiores fugiat aliquid autem quaerat incidunt!</div>
+            <div className='nws-block__date'>2024-04-08</div>
+          </div>
+          <div className="news-block">
+            <img  src="https://tengrinews.kz/userdata/news/2024/news_531610/thumb_m/photo_468156.jpeg" className='news-block__img' alt='news'></img>
+            <h2 className='news-block__title'>Lorem ipsum dolor sit</h2>
+            <div className='news-block__subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rem atque dicta maiores magnam laborum odio quaerat, possimus molestiae nostrum repudiandae. Illum exercitationem doloremque, asperiores fugiat aliquid autem quaerat incidunt!</div>
+            <div className='nws-block__date'>2024-04-08</div>
+          </div>
+          <div className="news-block">
+            <img  src="https://tengrinews.kz/userdata/news/2024/news_531610/thumb_m/photo_468156.jpeg" className='news-block__img' alt='news'></img>
+            <h2 className='news-block__title'>Lorem ipsum dolor sit</h2>
+            <div className='news-block__subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rem atque dicta maiores magnam laborum odio quaerat, possimus molestiae nostrum repudiandae. Illum exercitationem doloremque, asperiores fugiat aliquid autem quaerat incidunt!</div>
+            <div className='nws-block__date'>2024-04-08</div>
+          </div>
+          <div className="news-block">
+            <img  src="https://tengrinews.kz/userdata/news/2024/news_531610/thumb_m/photo_468156.jpeg" className='news-block__img' alt='news'></img>
+            <h2 className='news-block__title'>Lorem ipsum dolor sit</h2>
+            <div className='news-block__subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rem atque dicta maiores magnam laborum odio quaerat, possimus molestiae nostrum repudiandae. Illum exercitationem doloremque, asperiores fugiat aliquid autem quaerat incidunt!</div>
+            <div className='nws-block__date'>2024-04-08</div>
+          </div>
+        </div>
       </div>
-      
     </>
   )
 }
