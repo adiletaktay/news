c33c2b33dd6e8c1fe9b22bd11b93a4b668352b42
ADI <adiletaktay@gmail.com>
2024-07-10 11:46:54 +0500

    code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './Home'
-import MainLayout from './MainLayout'
-import { NotFound } from './NotFound'
+import React, { Suspense } from "react"
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./Home"
+import MainLayout from "./MainLayout"
 
-import './App.scss'
+import "./App.scss"
+
+const NotFound = React.lazy(() => import("./NotFound").then(({ NotFound }) => ({ default: NotFound })))
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route path='' element={<Home/ >}/>
-        <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   )
